037ef8cd1c8c6e23aa9afbcbd04ff5e865323bd2
rmasutani <masutanirugby@gmail.com>
2021-04-03 22:10:43 +0900

    add ranking

--- my_nlp/common/util.py
+++ my_nlp/common/util.py
@@ -45,6 +45,32 @@ def cos_similarity(x, y, eps=1e-8):
     return np.dot(nx, ny)
 
 
+def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    if query not in word_to_id:
+        print("%s not found" % query)
+        return
+    
+    print("\n[query]" + query)
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+
+    vocab_size = len(id_to_word)
+    similarity = np.zeros(vocab_size)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(word_matrix[i], query_vec)
+    
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if id_to_word[i] == query:
+            continue
+        print("%s: %s" % (id_to_word[i], similarity[i]))
+
+        count += 1
+        if count >= top:
+            return 
+
+
+
 if __name__ == '__main__':
     text = "You say goodbye and I say hello."
     corpus, word_to_id, id_to_word = preprocess(text)
