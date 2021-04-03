3e901d261b536bfe2cdf812f83ae0693ec5308ff
rmasutani <masutanirugby@gmail.com>
2021-04-03 21:56:00 +0900

    Add co_matrix and cos similarity

--- my_nlp/common/util.py
+++ my_nlp/common/util.py
@@ -19,9 +19,37 @@ def preprocess(text):
     return corpus, word_to_id, id_to_word
 
 
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size+1):
+            left_idx = idx - i
+            right_idx = idx + i 
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+            
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+    
+    return co_matrix
+
+
+def cos_similarity(x, y, eps=1e-8):
+    nx = x / np.sqrt(np.sum(x**2) + eps)
+    ny = y / np.sqrt(np.sum(y**2) + eps)
+    return np.dot(nx, ny)
+
+
 if __name__ == '__main__':
     text = "You say goodbye and I say hello."
     corpus, word_to_id, id_to_word = preprocess(text)
-    print(corpus)
-    print(word_to_id)
-    print(id_to_word)
+    co_mat = create_co_matrix(corpus, len(word_to_id))
+
+    c0 = co_mat[0]
+    c1 = co_mat[4]
+    print(cos_similarity(c0, c1))
